c31fe6245d39ccea686fef0670564698a2a2f2c4
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2025-01-08 15:54:28 +0330

    Create search.tsx

---
+++ app/components/search.tsx
@@ -0,0 +1,11 @@
+import Form from 'next/form'
+const SearchFrom=()=>{
+    return(
+
+        <Form action="/search" scroll={false} className='search-form'>
+
+    </Form>
+
+    )
+}
+export default SearchFrom  
