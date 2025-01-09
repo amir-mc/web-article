aa1707e4def050ba3ea0332dc7ffe9cecedd0a85
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2025-01-09 16:00:04 +0330

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
 import { Search } from "lucide-react";
-import SearchFrom from "../components/search";
+import SearchFrom from "../../components/search";
 
 
-export default function Home() {
+
+export default async function Home({searchParams}:{
+  searchParams:Promise<{query?:string}>
+}) 
+
+{ 
+const query=(await searchParams).query
   return (
     <>
     <section className="pink_container">
@@ -13,7 +19,7 @@ export default function Home() {
               news
             </p>
             <Search/>
-            <SearchFrom/>
+            <SearchFrom query={query}/>
     </section>
 
     
