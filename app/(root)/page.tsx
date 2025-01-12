8f27a1888d60a8c1dcb65022f3bf76f44e53dfa6
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2025-01-12 21:16:51 +0330

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import { Search } from "lucide-react";
 import SearchFrom from "../../components/search";
+import Sturtup from "@/components/startupfile";
 
 
 
@@ -8,6 +9,16 @@ export default async function Home({searchParams}:{
 }) 
 
 { 
+  const posts=[{
+    _createAt:new Date(),
+    view:21,
+    author:{_id:1,name:'Amir'},
+    _id:1,
+    descroption:'this is a test descroption of this post',
+    category:'Investing',
+    imge:"https://www.builderspace.com/wp-content/uploads/2022/05/modern-buildings-1568x1045.jpg",
+    title:"Building "
+  }]
 const query=(await searchParams).query
   return (
     <>
@@ -21,6 +32,22 @@ const query=(await searchParams).query
             <Search/>
             <SearchFrom query={query}/>
     </section>
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query ? `search resualt for "${query}" `:'AF fact'}
+      </p>
+      <ul className="mt-7 card_grid">
+          {
+            posts.length>0?(
+              posts.map((post:startupcard)=>(
+              <Sturtup key={post?._id} post={post}/>
+              ))
+            ):(
+            <p className="no-results">NOT FOUND</p>
+            )
+          }
+        </ul>
+      </section>
 
     
     </>
