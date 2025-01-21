70762c1e707a76fa9545867320ae03ef153c0217
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2025-01-21 22:42:09 +0330

    Update startupfile.tsx

--- components/startupfile.tsx
+++ components/startupfile.tsx
@@ -5,14 +5,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-const Sturtup=({post}:{post:startupcard})=>{
-const {_createAt,view,image,author,descroption,_id,category,title}=post
+
+
+import { Startup,Author } from "@/sanity/types";
+
+export type Startupcard=Omit<Startup,"author">&{author?:Author}
+const Sturtup=({post}:{post:Startupcard})=>{
+const {_createdAt,view,image,author,description,_id,category,title}=post
     return(
         <>
         <li className="startup-card group">
             <div className="flex-between">
                 <p className="stratup_card_date"> 
-                    {formatDate(_createAt)}
+                    {formatDate(_createdAt)}
                 </p>
                 <div className="flex gap-1.5">
                 <EyeIcon className="size-6 "></EyeIcon>
@@ -21,9 +26,9 @@ const {_createAt,view,image,author,descroption,_id,category,title}=post
             </div>
             <div className="flex-between mt-5 gap-5">
                 <div className="flex-1">
-                <Link href={`/users/${author._id}`}>
+                <Link href={`/users/${author?._id}`}>
                 <p className="text-16-medium line-clamp-1">
-                    {author.name}
+                    {author?.name}
                 </p>
                     </Link>
                     <Link href={`/startup/${_id}`}>
@@ -33,19 +38,19 @@ const {_createAt,view,image,author,descroption,_id,category,title}=post
                     </Link>
 
                 </div>
-                <Link href={`/users/${author._id}`}>
+                <Link href={`/users/${author?._id}`}>
                 <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full"></Image>
                 </Link>
 
             </div>
             <Link href={`/startup/${_id}`}>
                 <p className="startup-card_desc">
-                {descroption}
+                {description}
                 </p>
                 <Image width={200} height={200} src={image} alt="placeholder" className="startup-card_img"/>
              </Link>
                 <div className="flex-between gap-3 mt-5">
-                        <Link href={`/?query=${category.toLowerCase()}`}>
+                        <Link href={`/?query=${category?.toLowerCase()}`}>
                         <p className="text-16-medium">{category}</p>
                           </Link>
 
